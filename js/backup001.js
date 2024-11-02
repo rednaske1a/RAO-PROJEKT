b3814ecc37a39381da120ae7a3ccd0e6ba980b46
Aleksander Primožič Gogala <aleksander@apg.si>
2024-11-02 18:33:58 +0200

    Revert backup file

--- js/backup001.js
+++ js/backup001.js
@@ -3,7 +3,7 @@ const gameData = {
         "level1": [
             {
                 "id": 0,
-                "name": "CaveGround23",
+                "name": "CaveGround",
                 "pos": { "x": 0, "y": 0 },
                 "size": { "w": 1000, "h": 100 },
                 "components": [
